85469e45a5f643f583838121007f869c4469e175
jellelangen <50830538+jellelangen@users.noreply.github.com>
2023-07-02 16:25:06 +0200

    confusion matrix commit

--- Network_architecture.py
+++ Network_architecture.py
@@ -2,15 +2,19 @@ import math
 
 import tensorflow as tf
 from tensorflow import keras
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 
 def plot_histories(loss, val_loss, hyperparameter_values, std_loss, std_val_loss):
     plt.figure()
-    plt.xticks([0,1,2,3,4], ['{:.0e}'.format(x) for x in hyperparameter_values])
+    plt.xticks([0, 1, 2, 3, 4, 5], ['{:.0e}'.format(x) for x in hyperparameter_values])
 
-    plt.errorbar([0, 1, 2, 3, 4], loss, yerr = std_loss, color = 'blue', capsize=5, elinewidth=3)
-    plt.errorbar([0, 1, 2, 3, 4], val_loss, yerr = std_val_loss, color = 'orange', capsize=5)
+    plt.errorbar([0, 1, 2, 3, 4, 5], loss, yerr = std_loss, color = 'blue', capsize=5, elinewidth=3)
+    plt.errorbar([0, 1, 2, 3, 4, 5], val_loss, yerr = std_val_loss, color = 'orange', capsize=5)
     plt.title("Loss of the model")
     plt.legend(["loss", "val_loss"])
     plt.xlabel('alpha value')
@@ -26,29 +30,72 @@ X, y = load_data()
 split = int(len(X)*0.8)
 X_train, X_test = X[:split],X[split:]
 y_train, y_test = y[:split],y[split:]
-hyperparameter_values = [0.01, 0.001, 0.0001, 0.00001, 0.000001, 0]
-std_val_loss = []
-std_loss =[]
-evaluation = []
-loss = []
-val_loss = []
 
-for hyperparameter_value in hyperparameter_values:
-    print(hyperparameter_value)
-    model = keras.Sequential()
-    model.add(keras.layers.Dense(64, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
-    model.add(keras.layers.Dense(48, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
-    model.add(keras.layers.Dense(32, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
-    model.add(keras.layers.Dense(5, activation='softmax'))
-    model.compile(loss = "categorical_crossentropy",
-    optimizer = keras.optimizers.Adam(learning_rate = 0.001), metrics=["categorical_accuracy"])
-    history = model.fit(X_train, y_train, epochs=500, verbose = False, validation_split=0.2)
-    std_val_loss.append(np.std(history.history['loss'][-30:])/math.sqrt(30))
-    std_loss.append(np.std(history.history['loss'][-30:])/math.sqrt(30))
-    loss.append(sum((history.history['loss'][-30:])) / 30)
-    val_loss.append(sum((history.history['val_loss'][-30:])) / 30)
+#Commented code below was used for hyperparameter tuning.
+# hyperparameter_values = [0.01, 0.001, 0.0001, 0.00001, 0.000001, 0]
+# std_val_loss = []
+# std_loss =[]
+# evaluation = []
+# loss = []
+# val_loss = []
+#
+# for hyperparameter_value in hyperparameter_values:
+#     print(hyperparameter_value)
+#     model = keras.Sequential()
+#     model.add(keras.layers.Dense(64, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
+#     model.add(keras.layers.Dense(48, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
+#     model.add(keras.layers.Dense(32, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(hyperparameter_value)))
+#     model.add(keras.layers.Dense(5, activation='softmax'))
+#     model.compile(loss = "categorical_crossentropy",
+#     optimizer = keras.optimizers.Adam(learning_rate = 0.001), metrics=["categorical_accuracy"])
+#     history = model.fit(X_train, y_train, epochs=500, verbose = False, validation_split=0.2)
+#     std_val_loss.append(np.std(history.history['loss'][-30:])/math.sqrt(30))
+#     std_loss.append(np.std(history.history['loss'][-30:])/math.sqrt(30))
+#     loss.append(sum((history.history['loss'][-30:])) / 30)
+#     val_loss.append(sum((history.history['val_loss'][-30:])) / 30)
+#
+#
+# plot_histories(loss, val_loss, hyperparameter_values, std_loss, std_val_loss)
+# plt.show()
+
+checkpoint_path = "Saved_models/model.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+optimal_alpha = 0.000001
+model = keras.Sequential()
+model.add(keras.layers.Dense(64, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(optimal_alpha)))
+model.add(keras.layers.Dense(48, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(optimal_alpha)))
+model.add(keras.layers.Dense(32, activation='sigmoid', kernel_regularizer=keras.regularizers.L2(optimal_alpha)))
+model.add(keras.layers.Dense(5, activation='softmax'))
+model.compile(loss = "categorical_crossentropy",
+optimizer = keras.optimizers.Adam(learning_rate = 0.001), metrics=["categorical_accuracy"])
+
+history = model.fit(X_train, y_train, epochs=500, callbacks=[cp_callback], verbose = 1)
 
 
-plot_histories(loss, val_loss, hyperparameter_values, std_loss, std_val_loss)
+
+
+
+y_pred = model.predict(X_test)
+y_true = y_test
+matrix = multilabel_confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+print(matrix)
+
+labels = ["N", "L", "R", "V", "/"]
+
+cm = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+
+disp.plot(cmap=plt.cm.Blues)
+
+
+plt.figure()
+plt.plot(history.history["loss"], color='blue')
+plt.legend(["loss"])
+plt.xlabel("epochs")
+plt.ylabel("training loss")
 plt.show()
-
